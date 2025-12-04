9bf1f0b09d5bd1d921aa4ecf25a8a802e4ca9e87
tiennm99 <tiennm99@outlook.com>
2025-12-04 15:38:36 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -73,7 +73,7 @@ export default {
         );
 
       } catch (error) {
-        console.error(error);
+        console.log(error);
         return Response.json(
           { error: error.message },
           { status: 500 }
